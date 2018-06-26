36c51e4bb55a0daf49ca5c52b4a9c02d5699dfe2
vic lotana <viclotana@gmail.com>
2018-06-26 18:40:23 +0100

    interpolation

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="test">
         <h1>{{title}}</h1>
+        <p>{{user.firstName}}</p>
     </div>
 </template>
+
+
 <script>
 export default {
     name: 'test',
     data(){
         return{
-            title: 'Hello Vic'
+            title: 'Hello Man',
+            user: {
+                firstName: 'Lotanna',
+                lastName: 'Nwose'
+            }
         }
     }
 }
