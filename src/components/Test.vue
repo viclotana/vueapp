9e8183b8aba41ace7806be7b0d59af43ae3bdff9
vic lotana <viclotana@gmail.com>
2018-06-26 22:14:42 +0100

    v-bind

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="test">
+        <input type="text" v-model="title"><br />
         <h1>{{title}}</h1>
         <p v-if="showName">{{user.firstName}}</p>
         <p v-else>Nobody</p>
         <ul>
-            <li v-for="item in items">{{item.title}}</li>
+            <li v-for="item in items" v-bind:key="item">{{item.title}}</li>
         </ul>
     </div>
 </template>
